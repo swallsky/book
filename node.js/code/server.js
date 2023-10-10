96300522d861e91094ffd45740d8ca65be244b16
jackXu <xujinzhang@372163.com>
2023-10-10 17:56:53 +0800

    code fixed

--- node.js/code/server.js
+++ node.js/code/server.js
@@ -1,14 +1,16 @@
-var http = require('http');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-http.createServer(function(request,response){
+const server = http.createServer(function(request,response){
     //发送HTTP头部
     // HTTP状态值 200
     // 返回文本
     response.writeHead(200,{'Content-Type':'text/plain'});
     //发送响应数据
     response.end("Hello World\n");
-}).listen(8888);
+});
 
-
-//终端打印如下信息
-console.log("Server running at http://127.0.0.1:8888/");
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
