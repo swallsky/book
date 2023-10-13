997a910948b6a795f8911fa8643e7299c293b608
jackXu <xujinzhang@372163.com>
2023-10-13 11:32:04 +0800

    exports module.exports添加

---
+++ node.js/code/mods/md-hello.js
@@ -0,0 +1,9 @@
+module.exports = function Hello(){
+    var name;
+    this.setName = function(thyName){
+        name = thyName;
+    };
+    this.sayHello = function(){
+        console.log("Hello "+name);
+    };
+};
---
+++ node.js/code/mods/md-test.js
@@ -0,0 +1,4 @@
+const Hello = require("./md-hello");
+const hello = new Hello();
+hello.setName("By Modules");
+hello.sayHello();
